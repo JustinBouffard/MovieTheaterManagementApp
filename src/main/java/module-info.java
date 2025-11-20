1df894f2e496f0978bb6c3a4eaca69e9f92f48c0
JustinBouffard <bouffardjustin@gmail.com>
2025-11-20 10:04:36 -0500

    Fixed project structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,5 @@ module com.example.theaterproject {
 
     opens com.example.theaterproject to javafx.fxml;
     exports com.example.theaterproject;
+    opens com.example.theaterproject.Controllers to javafx.fxml;
 }
