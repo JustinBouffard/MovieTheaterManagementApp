91bc709d4da1ccfdb873a364eca3447147b7d604
Serena <serena.shushack@stu.crc-lennox.qc.ca>
2025-11-20 09:17:23 -0500

    Fixed main view, added basic card logic for the movie cards.

--- TheaterProject/src/main/java/module-info.java
+++ TheaterProject/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.theaterproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.theaterproject to javafx.fxml;
