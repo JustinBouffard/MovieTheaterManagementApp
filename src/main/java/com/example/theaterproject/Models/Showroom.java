6efbe0460c0409e763f100ef155756d66159c82c
pfelix7 <perrefelix@gmail.com>
2025-11-20 09:28:47 -0500

    Started the Showroom class

--- src/main/java/com/example/theaterproject/Models/Showroom.java
+++ src/main/java/com/example/theaterproject/Models/Showroom.java
@@ -1,4 +1,29 @@
 package com.example.theaterproject.Models;
 
 public class Showroom {
+    private String aName;
+    private int aCapacity;
+
+    public Showroom (String pName, int pCapacity) {
+        this.aName = pName;
+        this.aCapacity = pCapacity;
+
+    }
+
+    private String getShowroomName() {
+        return this.aName;
+    }
+
+    private int getShowroomCapacity() {
+        return this.aCapacity;
+    }
+
+    private void setShowroomName(String pName) {
+        this.aName = pName;
+    }
+
+    private void setShowroomCapacity(int pCapacity) {
+        this.aCapacity = pCapacity;
+    }
+
 }
