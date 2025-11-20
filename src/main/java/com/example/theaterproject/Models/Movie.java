e63f15bdee4190274d1e5d858402b0e7cd181f57
JustinBouffard <bouffardjustin@gmail.com>
2025-11-20 09:04:37 -0500

    Made basic movie class functionality (some attributes could be removed)

--- src/main/java/com/example/theaterproject/Models/Movie.java
+++ src/main/java/com/example/theaterproject/Models/Movie.java
@@ -1,4 +1,34 @@
 package com.example.theaterproject.Models;
 
 public class Movie {
+    private String aGenre;
+    private String aTitle;
+    private String aDirector;
+    private String aYear;
+    private String aDescription;
+
+    public Movie(String aGenre, String aTitle, String aDirector, String aYear, String aDescription) {
+        this.aGenre = aGenre;
+        this.aTitle = aTitle;
+        this.aDirector = aDirector;
+        this.aYear = aYear;
+        this.aDescription = aDescription;
+    }
+    
+    public String getTitle() {
+        return aTitle;
+    }
+
+    public String getGenre() {
+        return aGenre;
+    }
+
+    public String getDescription() {
+        return aDescription;
+    }
+
+    @Override
+    public String toString() {
+        return aTitle + " " + aGenre + " " + aDirector + " " + aYear + " " + aDescription;
+    }
 }
