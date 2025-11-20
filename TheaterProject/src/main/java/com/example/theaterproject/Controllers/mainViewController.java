f3b458aa8b05c31e5c98892dde861d6fe2cc6fbd
Serena <serena.shushack@stu.crc-lennox.qc.ca>
2025-11-20 09:12:29 -0500

    The main view and main view controller, started.

---
+++ TheaterProject/src/main/java/com/example/theaterproject/Controllers/mainViewController.java
@@ -0,0 +1,10 @@
+package com.example.theaterproject.Controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class mainViewController {
+    }
