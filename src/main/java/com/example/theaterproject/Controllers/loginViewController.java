454520a0ffe2c09302dc04ef38c2e5ea220f2444
Serena <Serena.shushack@stu.crc-lennox.qc.ca>
2025-11-28 10:38:01 -0500

    Fixing views and adding basic controller functions

--- src/main/java/com/example/theaterproject/Controllers/loginViewController.java
+++ src/main/java/com/example/theaterproject/Controllers/loginViewController.java
@@ -1,7 +1,17 @@
 package com.example.theaterproject.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class loginViewController {
     @FXML
@@ -10,6 +20,35 @@ public class loginViewController {
     @FXML
     private Button newAccountButton;
 
+    @FXML
+    private TextField usernameTextField;
 
+    @FXML
+    private PasswordField passwordField;
 
+    private void onSignInButtonClick() {
+
+    }
+
+    private void onNewAccountButtonClick(ActionEvent pEvent) {
+        try {
+            // Load the new window
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("new-account-view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Create the new stage
+            Stage newStage = new Stage();
+            newStage.setTitle("New Account");
+            newStage.setScene(new Scene(root, 480, 350));
+            newStage.show();
+
+            // ---- Close current window ----
+            Stage currentStage = (Stage) ((Node) pEvent.getSource()).getScene().getWindow();
+            currentStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.theaterproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.desktop;
 
 
     opens com.example.theaterproject to javafx.fxml;
