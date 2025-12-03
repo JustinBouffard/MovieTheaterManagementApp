0bef0c6a1e9f774baa9e6196bd744267e6ada9b6
pfelix7 <perrefelix@gmail.com>
2025-12-03 10:24:12 -0500

    Removed unused imports
    Remove unused imports from NewAccount and StatsView controllers

--- src/main/java/com/example/theaterproject/Controllers/StatsViewController.java
+++ src/main/java/com/example/theaterproject/Controllers/StatsViewController.java
@@ -7,12 +7,8 @@ import com.example.theaterproject.Services.ShowroomService;
 import com.example.theaterproject.Services.UIService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
